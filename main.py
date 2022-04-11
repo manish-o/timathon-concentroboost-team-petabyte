03e14157d77ac681cbe2fd36f63e80749b4ea5cf
manish-o <80611918+manish-o@users.noreply.github.com>
2022-04-11 22:49:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from statistics import mode
+import eel
+from numpy import size
+import time
+
+eel.init("D://Manish//Intel self projects//timathon//eel")
+
+@eel.expose
+def lol():
+    i =[]
+
+eel.start("html/main.html")
